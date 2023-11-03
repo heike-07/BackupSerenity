2eeacbef1386c97957528a41e8e6e3dd106e5f67
xiaojun <dongbojun@ictrui.com>
2023-11-03 16:09:52 +0800

    wip 🕔 -  "更新go版本封装代码"

--- Backup_Mydumper_MultiThread_Database_All.go
+++ Backup_Mydumper_MultiThread_Database_All.go
@@ -20,6 +20,37 @@ func main() {
 # CALL Backup_Mydumper_MultiThread_Database_All.conf
 source conf/Backup_Mydumper_MultiThread_Database_All.conf
 
+If_Docker(){
+    # Principle
+    # IFIST Is Docker
+    # df -h | grep -e overlay | awk '{print $1}'
+    # ll -a / | grep -e .dockerenv | awk '{print $9}'
+    DockerType=$(df -h | grep -e overlay | awk '{print $1}')
+
+    if [[ $DOCKER_TYPE == $DockerType ]] ;then
+       echo "检测到/文件类型为overlay，判断为Docker容器环境"
+       If_FOLDER
+       else
+       echo "没有检测到/文件类型为overlay，当前环境不是Docker容器环境"
+       If_NFS
+    fi
+
+}
+
+If_FOLDER(){
+   # Principle
+   # IFIST FOLDER NFS_LINK_DISK
+   # ll -a / | grep -e NFS_LINK_DISK | awk '{print $9}'
+   Folder=$(ls -la / | grep -e NFS_LINK_DISK | awk '{print $9}')
+
+   if [[ $MYSQL_Nfs_DiskDir == $Folder ]] ;then
+      echo "发现文件夹 $Folder，正在继续执行脚本……"
+      If_Save_Url
+      else
+      echo "没有发现文件夹 $MYSQL_Nfs_DiskDir，请检查环境是否创建文件夹/$MYSQL_Nfs_DiskDir"
+   fi
+}
+
 If_NFS(){
     # Principle
     # IFIST NFS
@@ -43,10 +74,10 @@ If_Mydumper(){
     # whereis mydumper | wc -l  Y=1  F=0
     mydumper_status=$(whereis mydumper | wc -l)
     if [[ $mydumper_status -eq 0 ]] ;then
-	echo "没有发现mydumper程序,请确认该服务器是否为备份中控."
+	    echo "没有发现mydumper程序,请确认该服务器是否为备份中控."
     	else
-        echo "已发现mydumper程序,该服务器为备份中控服务器,正在执行拉取备份脚本……"
-	If_NFS
+      echo "已发现mydumper程序,该服务器为备份中控服务器,正在执行拉取备份脚本……"
+	    If_Docker
     fi
 }
 
